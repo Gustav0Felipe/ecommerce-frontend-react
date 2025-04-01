5defe2615d0e231baa317acdcda09ffb6f982bd5
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2025-04-01 18:20:51 -0300

    Menu Estilizado para melhor responsividade e logica, opções como perfil aparecem apenas ao cliente logado

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -2,6 +2,7 @@ import { Component, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import JavaIcon from '../../assets/favicon.ico'
 import { UserContext } from '../../context/userContext';
+import "./menu.css"
 
 function Menu (){
 	
@@ -37,17 +38,29 @@ function Menu (){
 			<Link to="/loja/"><img alt="Icone do Java" src={JavaIcon} id="logo"/></Link>
 			<nav id="menu">
 				<ul>
-					<li><Link to={"/loja/"}> Ofertas do Dia </Link></li>
-					<li><Link to={"/loja/login"}> Entre </Link></li>
-					<li><Link to={"/loja/perfil"}> Minha conta </Link></li>
-					<li><Link to={"/loja/cadastro"}> Crie a sua conta </Link></li>
-					<li><Link to={"/loja/cart"}>
-							<span id="cart" className="material-symbols-outlined">shopping_cart</span> 
-						</Link>
-					</li>
 					{
 					user.role == "ADMIN" && 
 					<li><Link to="/loja/admin">Admin</Link></li> 
+					}
+					<li><Link to={"/loja/"}> Ofertas do Dia </Link></li>
+					
+					{!user.id_user && <>
+							<li><Link to={"/loja/login"}> Entre </Link></li>
+							<li><Link to={"/loja/cadastro"}> Crie a sua conta </Link></li>
+							</>
+					}
+					<li><Link to={"/loja/cart"}>
+							<span className="material-symbols-outlined">shopping_cart</span> 
+						</Link>
+					</li>
+					{user.id_user && 
+					<li>
+						<Link to={"/loja/perfil"} id="menu_perfil_icon">
+						<span className="material-symbols-outlined">
+						account_circle
+						</span>
+						</Link>
+					</li>
 					}
 				</ul>
 			</nav>
