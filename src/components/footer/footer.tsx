481f4392d4bc929c0237940d56214600c58377e1
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2025-04-03 13:09:43 -0300

    Footer Organizado

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,14 +3,15 @@ import { GithubLogo } from '@phosphor-icons/react';
 export function Footer(){
     return(
         <>
-        <footer className="bg-[--cor--laranja--0] text-white py-4">
+        <footer className="bg-[--cor--laranja--0] text-white text-center py-4">
           <div className="container mx-auto flex flex-col items-center text-center space-y-3">
             <div className="flex items-center space-x-2 text-sm">
-              <a href="https://github.com/meGustaRacalu" target="_blank" rel="noopener noreferrer" className="hover:underline flex items-center">
+              <a href="https://github.com/Gustav0Felipe" target="_blank" rel="noopener noreferrer" className="hover:underline flex items-center">
                 <GithubLogo className='text-white' size={30} weight="bold"/>
-                <span className='ml-1 text-white'>GitHub | Site feito por Gustavo Felipe  </span>
+                <span className='ml-1 text-white'>GitHub | feito por Gustavo Felipe  </span>
               </a>
             </div>
+                <span className='ml-1 text-white'>Site Sem Proposito Comercial</span>
               <p>• © {new Date().getFullYear()} Ecommerce</p>
           </div>
         </footer>
